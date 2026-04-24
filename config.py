ee1d6b409043cd25833c1c9b313f0dc9b03acf1f
yakujaae-afk <yakujaae@gmail.com>
2026-04-23 23:25:01 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID    = os.environ.get("30296254", "")
+API_HASH  = os.environ.get("c2b5306f4ccd2d795405a026c10b4c62", "")
+BOT_TOKEN = os.environ.get("8697293437:AAFwdPxKY-MaVw22-e_Z7ue0yHmbm-DGCs0", "")
 AUTH_USERS = os.environ.get("AUTH_USERS","7660916897"")
