4029abdccbbe325b6551f90b31ef383688796098
yakujaae-afk <yakujaae@gmail.com>
2026-04-23 23:10:45 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8631709476:AAEcmTI71C7aE3W_iBtzwF2WwRNjMWZQ93E")
-    API_ID = int(os.environ.get("API_ID", " 30296254"))
-    API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
-    AUTH_USERS = "7660916897"
-
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
